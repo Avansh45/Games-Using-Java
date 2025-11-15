38e47aeb03ca69c2f0996c8745291df462d23e98
Avansh Chadgal <avanshchadgalmh25@gmail.com>
2025-11-15 14:20:47 +0530

    Improved code with Exception Handling

--- GuessNum.java
+++ GuessNum.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 import java.util.Random;
 
 class Game{
@@ -35,25 +36,40 @@ public class GuessNum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Game g = new Game();
-        int userNum;
-        while((g.isCorrectNumber())==false){
-            g.setGuesses();
-            userNum = sc.nextInt();
-            g.userNum=userNum;
-            if(g.getRandom()>userNum){
-                System.out.println("you have entered smaller number");
-            }
-            else if(g.getRandom()<userNum){
-                System.out.println("you have entered greater number");
-            }
-            else{
-                break;
+        System.out.println(" Number has been choosen(0-100). Let's begin !");
+        System.out.print("Choose the number :");
+        
+        while(true){
+           try {
+                g.setGuesses(); 
+                
+                int userNum = sc.nextInt();
+                g.userNum = userNum;
+                
+                if (g.isCorrectNumber()) {
+                    System.out.println("\n--- CONGRATULATIONS! ---");
+                    break;
+                }
+                else if (userNum < g.getRandom()) {
+                    System.out.println("You entered a smaller number.");
+                    System.out.println("Try Again! \n------------------------------");
+                }
+                else {
+                    System.out.println("You entered a greater number.");
+                    System.out.println("Try Again! \n------------------------------");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("\nInvalid Input! Please enter a whole number only.");
+                System.out.println("------------------------------");
+                g.num--;
+                
+                sc.next(); 
             }
         }
         System.out.println("No.of Gusses: "+g.getGuesses());
         
         
-        System.out.println("Random Number Was "+g.getRandom());
+        System.out.println("Choosen Number Was "+g.getRandom());
         sc.close();
     }
 }
